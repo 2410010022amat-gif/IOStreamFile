026ed6aa69cb882601b567250b8995fb85fca9fd
2410010022amat-gif <2410010022amat@gmail.com>
2026-06-23 12:45:31 +0800

    Selesai Bagian 3: Multi-Class

---
+++ src/bagian3/kontak/MainKontak.java
@@ -0,0 +1,49 @@
+package bagian3.kontak;
+
+public class MainKontak {
+
+    public static void main(String[] args) {
+
+        BukuKontak buku = new BukuKontak("kontak.txt");
+
+        buku.tambahKontak(
+                new Kontak(
+                        "Andi",
+                        "0811111",
+                        "andi@gmail.com"));
+
+        buku.tambahKontak(
+                new Kontak(
+                        "Budi",
+                        "0822222",
+                        "budi@gmail.com"));
+
+        buku.tambahKontak(
+                new Kontak(
+                        "Citra",
+                        "0833333",
+                        "citra@gmail.com"));
+
+        buku.tampilkanSemua();
+
+        // LATIHAN MANDIRI NO 1
+        buku.cariKontak("Budi");
+
+        // LATIHAN MANDIRI NO 3
+        buku.hapusKontak("Budi");
+
+        System.out.println("\nSetelah penghapusan:");
+        buku.tampilkanSemua();
+
+        buku.simpanKeBerkas();
+
+        System.out.println();
+
+        BukuKontak bukuLain = new BukuKontak("kontak.txt");
+
+        bukuLain.muatDariBerkas();
+        bukuLain.tampilkanSemua();
+
+        System.out.println("Jumlah kontak: " + bukuLain.jumlahKontak());
+    }
+}
